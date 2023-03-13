0208c811216e4c421b73ae62367b1833608c3e97
hikapyon <hikaru.okafuji@gmail.com>
2023-03-13 20:53:57 +0900

    second commit

--- black-jack.py
+++ black-jack.py
@@ -15,18 +15,18 @@ deck = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11] * 4
 random.shuffle(deck)
 
 print(f'{"*"*58} \n Welcome to the game Casino - BLACK JACK ! \n{"*"*58}')
-sleep(2)
+sleep(1)
 print("So Finally You Are Here To Accept Your Fate")
-sleep(2)
+sleep(1)
 print("I Mean Your Fortune")
-sleep(2)
+sleep(1)
 print("Lets Check How Lucky You Are  Wish You All The Best")
-sleep(2)
+sleep(1)
 print("Loading---")
-sleep(2)
+sleep(1)
 
 print("Still Loading---")
-sleep(2)
+sleep(1)
 print(
     "So You Are Still Here Not Gone I Gave You Chance But No Problem May Be You Trust Your Fortune A Lot \n Lets Begin Then"
 )
